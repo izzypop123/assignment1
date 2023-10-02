1ad2c704519a03d14ae2cf2269fcc2f7c3ea69ec
izzypop123 <146539306+izzypop123@users.noreply.github.com>
2023-10-02 05:33:14 -0700

    Add files via upload

---
+++ header.js
@@ -0,0 +1,25 @@
+
+
+// function myFunction () {
+
+//     const open = document.getElementById('link')
+
+//     if (open.style.display === 'block'){
+
+//         open.style.display == 'none';
+        
+//     }else{
+//         open.style.display = 'block'
+
+//     }
+
+// }
+
+function myFunction() {
+    var x = document.getElementById("link");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
